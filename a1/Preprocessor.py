c07b10667070a6a8f56729f2158388e28f61c3eb
mpatel4 <mpatel4@uottawa.ca>
2023-02-13 20:31:23 -0500

    temp preprocessor

--- a1/Preprocessor.py
+++ a1/Preprocessor.py
@@ -24,50 +24,20 @@ def do_preprocessor(filepath):
     
     punct_set = set(string.punctuation)
     
-    print(filepath)
-    
     document_word_dict = {}
     
     for filename in os.listdir(filepath):
-        p = Path(filepath+"/"+filename)
-        p.rename(p.with_suffix('.xml'))
-        
-        with open(filepath+"/"+filename+".xml", encoding = "utf-8-sig") as file:
+        with open("coll/"+filename, encoding = "utf-8") as file:
+            doc = file.read()
             
-            tree = ET.fromstring(file.read())
-            for node in tree.iter('text'):
-                print('\n')
-                
-            
-            
-            
-        pr = Path(filepath+"/"+filename+".xml")
-        pr.rename(p.with_suffix(''))
-    
+        tree = ET.parse(doc)
+        root = tree.getroot(tree)
+        textList = []
+        for text in root.iter('text'):
+            textList.append(text.text)
 
-
-    #Save to folder
-    #Important: When we test later, we only need to load the json file (No need to recreate a new file every query)
-    document_word_dict_path = "document_word_dict.json"
-    with open(document_word_dict_path, "w") as file:
-        json.dump(document_word_dict, file)  
-    
-    #Create a temporary dict for (document_word_dict)
-    tmp_dict = document_word_dict
-    #print(tmp_dict)
-
-    
-    
-
-    #Create document_word_count_dict
-    document_word_count_dict = {}
-    for doc in tmp_dict:
-        document_word_count_dict[doc] = Counter(tmp_dict[doc]);
-    #print(document_word_count_dict)
-
-
-    #Returns both dictionaries (document_word_dict) and (document_word_count_dict)
-    return document_word_dict, document_word_count_dict # only need tokenized dictionary
+        #print(len(textList))              
+        exit()
 
 if __name__=="__main__":
     main()
